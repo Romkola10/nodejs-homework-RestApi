4ce753ee7665009726c85f56061a4ec35f0e00a4
Romkola10 <57733600+Romkola10@users.noreply.github.com>
2023-03-05 16:42:08 +0200

    restApi first hw done

---
+++ middlewares/validation.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+const addPostValidation = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      })
+      .required(),
+    phone: Joi.string().alphanum().min(3).max(30).required(),
+  });
+
+  const validationResult = schema.validate(req.body);
+
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error.details,
+      code: 400,
+    });
+  }
+
+  next();
+};
+
+module.exports = { addPostValidation };
