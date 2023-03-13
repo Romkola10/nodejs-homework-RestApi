66bf77b2a6bdf4ea44e48e240c7e4b41b37b7c77
Romkola10 <57733600+Romkola10@users.noreply.github.com>
2023-03-13 05:22:28 +0200

    add verification with email and dockerfile

--- models/userModels.js
+++ models/userModels.js
@@ -28,10 +28,17 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
-
 
 const User = model("user", userSchema);
 
